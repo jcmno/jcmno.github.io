3388ce8102dec27be9f8d8d21b126a82f3fae2da
jcmno <jal.cmno@gmail.com>
2025-06-09 16:06:31 -0700

    Added a card for each degree and added SFU logo to each one

--- jcmnowebpage/src/components/Education/Education.jsx
+++ jcmnowebpage/src/components/Education/Education.jsx
@@ -1,14 +1,38 @@
 import React from 'react'
 import './Education.css'
+import sfulogo from '../../assets/sfu.png'
 
 const Education = () => {
   return (
     <div className = 'education'>
 
-        <hi>Education</hi>
+        <h1>Education</h1>
 
-        <h2 className = 'cmpt'>Post-baccalaureatte Diploma: Computing Science </h2>
+        <div className = 'educard'>
 
+        <img className = 'sfu' src = {sfulogo} alt = 'Simon Fraser University logo'></img>   
+
+        <ul className = 'eduTitle'>Post-Baccalaureate Diploma:</ul>
+        <ul className = 'eduTitle'>Computing Science </ul>
+        <div className = 'eduList'>
+        <li>2025 - PRESENT</li>
+        <li>Simon Fraser University</li>
+        
+        </div>
+        </div>
+
+        <div className = 'educard'>
+
+        <img className = 'sfu' src = {sfulogo} alt = 'Simon Fraser University logo'></img>   
+
+        <ul className = 'eduTitle'>Bachelor of Science:</ul>
+        <ul className = 'eduTitle'>Biological Sciences</ul>
+        <div className = 'eduList'>
+        <li>2015 - 2019</li>
+        <li>Simon Fraser University</li>
+        
+        </div>
+        </div>
 
 
       
