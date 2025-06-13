b3e0d43eef6f9a9454a4c2d24cb0e6fe17abc056
jcmno <jal.cmno@gmail.com>
2025-06-13 15:07:43 -0700

    Added the Hobbies section to the App.js file. Photos for 'Travel and Outdoors' section have been uploaded to the assets folder. Issue #12.

---
+++ jcmnowebpage/src/components/Hobbies/Hobbies.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Hobbies.css'
+import shibuya from '../../assets/Shibuya.jpg'
+
+const Hobbies = () => {
+  return (
+    <div>
+      <h1>Hobbies</h1>
+
+        <h2>Travel and Outdoors</h2>
+        <p>In my spare time, I enjoy going on trips and spending time outdoors. 
+            I believe it's important to stay active and travel while we are young.
+            One of my favorite trips I went on recently is Japan (pictured below). 
+            Visit my <a href = "https://www.instagram.com/john_camino/">Instagram</a> to see more trips.
+        </p>
+
+
+
+
+        <h2>Magic the Gathering</h2>
+        <p>I enjoy playing trading card game called Magic the Gatherinig.
+            It's a fun social activity that I have been actively participating in recently. This game also brings out my
+            creativity through deck building. Click <a href = "https://archidekt.com/u/retrospected">here</a> to see the decks I have been brewing up!
+        </p>
+
+    </div>
+  )
+}
+
+export default Hobbies
